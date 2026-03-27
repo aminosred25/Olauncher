70e5404ac63afe8df83ced33d69a02e6c203f484
aminosred25 <amine@MacBook-Air.local>
2026-03-27 17:12:35 +0000

    customizations

--- app/src/main/java/app/olauncher/ui/HomeFragment.kt
+++ app/src/main/java/app/olauncher/ui/HomeFragment.kt
@@ -255,7 +255,7 @@ class HomeFragment : Fragment(), View.OnClickListener, View.OnLongClickListener 
         binding.date.isVisible = Constants.DateTime.isDateVisible(prefs.dateTimeVisibility)
 
 //        var dateText = SimpleDateFormat("EEE, d MMM", Locale.getDefault()).format(Date())
-        val dateFormat = SimpleDateFormat("EEE, d MMM", Locale.getDefault())
+        val dateFormat = SimpleDateFormat("EEE, MMM dd", Locale.getDefault())
         var dateText = dateFormat.format(Date())
 
         if (!prefs.showStatusBar) {
@@ -302,7 +302,7 @@ class HomeFragment : Fragment(), View.OnClickListener, View.OnLongClickListener 
             populateScreenTime()
 
         val homeAppsNum = prefs.homeAppsNum
-        if (homeAppsNum == 0) return
+        if (homeAppsNum == 0) return showAppList(Constants.FLAG_LAUNCH_APP)
 
         binding.homeApp1.visibility = View.VISIBLE
         if (!setHomeAppText(binding.homeApp1, prefs.appName1, prefs.appPackage1, prefs.appUser1, prefs.isShortcut1, prefs.shortcutId1)) {
